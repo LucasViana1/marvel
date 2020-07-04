3e8a78c9777ba4ca330519b26800164d6972f82a
Lucas Viana <lucasviana112@gmail.com>
2020-07-04 15:58:44 -0300

    initial component structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// @flow
+import React from 'react';
+
+import './App.css';
+
+import Access from './pages/Access';
+
+function App() {
+  return (
+    <>
+      <Access />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Access/index.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const Access = () => {
+  return (
+    <section>
+      <h2>Acesso</h2>
+    </section>
+  );
+};
+
+export default Access;
