aaddd47241834833e910e40178db65a55fa5a086
Lucas Viana <lucasviana112@gmail.com>
2020-07-04 17:58:07 -0300

    global style

---
+++ src/styles/global.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: 0;
+  }
+
+  body{
+    -webkit-font-smothing: antialiased;
+  }
+
+  /* body, input, button{
+    font: 16px Roboto, sans-serif
+  } */
+
+  #app{
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 40px 20px;
+  }
+
+  button{
+    cursor: pointer;
+  }
+`;
