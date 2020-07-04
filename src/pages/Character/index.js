e5b3d5991456269a6c9976795d859944db064b91
Lucas Viana <lucasviana112@gmail.com>
2020-07-04 18:01:38 -0300

    character and home screens

---
+++ src/pages/Character/index.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const Character = () => {
+  return (
+    <section>
+      <h2>Character</h2>
+    </section>
+  );
+};
+
+export default Character;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const Home = () => {
+  return (
+    <section>
+      <h2>Home</h2>
+    </section>
+  );
+};
+
+export default Home;
