c79207cea3dc4eef393229b1e2adb89928d98dd8
Lucas Viana <lucasviana112@gmail.com>
2020-07-05 14:59:45 -0300

    initial characters actions

---
+++ src/store/modules/characters/actions.js
@@ -0,0 +1,60 @@
+// @flow
+export function charactersLoadRequest(privateKey: string, publicKey: string) {
+  return {
+    type: '@characters/LOAD_REQUEST',
+    payload: {
+      privateKey,
+      publicKey,
+    },
+  };
+}
+export function charactersLoadComicsRequest(characterId: number) {
+  return {
+    type: '@characters/LOAD_COMICS_REQUEST',
+    payload: {
+      characterId,
+    },
+  };
+}
+
+export function charactersLoadSuccess(
+  characters: any[],
+  timestamp: any,
+  publicKey: string,
+  hash: string,
+) {
+  return {
+    type: '@characters/LOAD_SUCCESS',
+    payload: {
+      characters,
+      timestamp,
+      publicKey,
+      hash,
+    },
+  };
+}
+export function charactersLoadUpdateSuccess(characters: any[]) {
+  return {
+    type: '@characters/LOAD_UPDATE_SUCCESS',
+    payload: {
+      characters,
+    },
+  };
+}
+export function charactersLoadComicsSuccess(comics: any[]) {
+  return {
+    type: '@characters/LOAD_COMICS_SUCCESS',
+    payload: {
+      comics,
+    },
+  };
+}
+
+export function charactersLoadUpdated(quantityPage: number) {
+  return {
+    type: '@characters/LOAD_UPDATED',
+    payload: {
+      quantityPage,
+    },
+  };
+}
