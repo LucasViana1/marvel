0ede1a5ab65361c9323e1e52e10a374249dd0f89
Lucas Viana <lucasviana112@gmail.com>
2020-07-05 15:00:03 -0300

    initial characters reducer

---
+++ src/store/modules/characters/reducer.js
@@ -0,0 +1,55 @@
+// @flow
+const INITIAL_STATE = {
+  characters: [],
+  timestamp: '',
+  publicKey: '',
+  hash: '',
+  comics: [],
+};
+
+const characters: any = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    // case '@characters/LOAD_REQUEST':
+    //   console.log('state - action load request');
+    //   console.log(state);
+    //   console.log(action);
+    //   return {
+    //     ...state,
+    //     // movies: action.payload.data,
+    //   };
+    case '@characters/LOAD_SUCCESS':
+      console.log('state - action load success');
+      console.log(state);
+      console.log(action);
+      return {
+        ...state,
+        characters: action.payload.characters,
+        timestamp: action.payload.timestamp,
+        publicKey: action.payload.publicKey,
+        hash: action.payload.hash,
+      };
+    case '@characters/LOAD_UPDATE_SUCCESS':
+      console.log('state - action load updates success');
+      console.log(state);
+      console.log(action);
+      return {
+        ...state,
+        characters: action.payload.characters,
+        // timestamp: action.payload.timestamp,
+        // publicKey: action.payload.publicKey,
+        // hash: action.payload.hash,
+      };
+    case '@characters/LOAD_COMICS_SUCCESS':
+      console.log('state - action LOAD_COMICS_SUCCESS');
+      console.log(state);
+      console.log(action);
+      return {
+        ...state,
+        comics: action.payload.comics,
+      };
+    default:
+      return state;
+  }
+};
+
+export default characters;
