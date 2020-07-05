ef0fd0d1817568892a32b5837d3f14acfe16446d
Lucas Viana <lucasviana112@gmail.com>
2020-07-05 14:45:12 -0300

    redux provider insertion

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@
 import React from 'react';
 
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import GlobalStyle from './styles/global';
 import Routes from './routes';
+
+import store from './store';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </Provider>
       <GlobalStyle />
     </>
   );
