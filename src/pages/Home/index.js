4324379a1e4c214bb9143b501323cdf12e9e8fd1
Lucas Viana <lucasviana112@gmail.com>
2020-07-05 14:48:49 -0300

    initial implementation of the character table and pagination

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,69 @@
+// @flow
 import * as React from 'react';
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  charactersLoadRequest,
+  charactersLoadUpdated,
+} from '../../store/modules/characters/actions';
+
+import { Container, CharacterTable, HeadText, Pagination } from './styles';
+
+import CharacterTableItem from '../../components/CharacterTableItem';
 
 const Home = () => {
+  const [actualPage, setActualPage] = useState(0);
+
+  const { characters } = useSelector(state => state.characters);
+
+  const dispatch = useDispatch();
+
+  // USADO APENAS PARA TESTES!!!
+  useEffect(() => {
+    dispatch(
+      charactersLoadRequest(
+        'b286c0cd5ce1c9aaca4414ee601aee3019f5e744',
+        'e3d2fee5996812a43cde053cb755b88b',
+      ),
+    );
+  }, [dispatch]);
+
+  function handlePage(pageNumber) {
+    // console.log('quantityPage');
+    // console.log(quantityPage);
+    const totalPages = actualPage + pageNumber;
+    dispatch(charactersLoadUpdated(totalPages));
+    setActualPage(totalPages);
+  }
+  console.log('teste');
+  console.log(useSelector(state => state));
+
   return (
-    <section>
-      <h2>Home</h2>
-    </section>
+    <Container>
+      <CharacterTable>
+        <thead>
+          <tr>
+            <HeadText>Nome</HeadText>
+            <HeadText>Descrição</HeadText>
+            <HeadText>Última atualização</HeadText>
+          </tr>
+        </thead>
+        <tbody>
+          {characters &&
+            characters.map(character => (
+              <CharacterTableItem key={character.id} character={character} />
+            ))}
+        </tbody>
+      </CharacterTable>
+      <Pagination>
+        <button type="button" onClick={() => handlePage(-10)}>
+          anterior
+        </button>
+        <button type="button" onClick={() => handlePage(10)}>
+          próximo
+        </button>
+      </Pagination>
+    </Container>
   );
 };
 
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.section``;
+export const CharacterTable = styled.table`
+  border: 1px solid black;
+`;
+export const HeadText = styled.th``;
+export const Pagination = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+`;
