6f8c248809935febd388bf72994b4b820eea1b5a
Lucas Viana <lucasviana112@gmail.com>
2020-07-06 09:25:37 -0300

    linking data request for the selected character

--- src/pages/Character/index.js
+++ src/pages/Character/index.js
@@ -2,38 +2,36 @@
 import * as React from 'react';
 import { useEffect } from 'react';
 
-// import md5 from 'md5';
-// import api from '../../services/api';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { charactersLoadComicsRequest } from '../../store/modules/characters/actions';
+import { useParams } from 'react-router-dom';
+import { loadCharacterById } from '../../store/modules/characters/actions';
+
+import ComicItem from '../../components/ComicItem';
 
 const Character = () => {
+  const { characterId } = useParams();
+
   const dispatch = useDispatch();
 
+  console.log('id');
+
+  console.log(characterId);
+
   console.log('data character screen');
-  console.log(useSelector(state => state));
+  console.log(useSelector(state => state.characters));
+
+  const { comics } = useSelector(state => state.characters);
 
   useEffect(() => {
-    // const timestamp = Math.floor(Date.now() / 1000);
-    // const privateKey = 'b286c0cd5ce1c9aaca4414ee601aee3019f5e744';
-    // const publicKey = 'e3d2fee5996812a43cde053cb755b88b';
-    // const hash = md5(timestamp + privateKey + publicKey);
-    // api
-    //   .get(
-    //     `characters/1010673/comics?ts=${timestamp}&apikey=${publicKey}&hash=${hash}&limit=10`,
-    //   )
-    //   .then(response => {
-    //     console.log('stories personagem');
-    //     console.log(response.data);
-    //     // console.log(response.data.data.results);
-    //   });
-    dispatch(charactersLoadComicsRequest(1010673));
-  }, [dispatch]);
+    // DISPATCH CHARACTER BY ID AND COMICS
+    dispatch(loadCharacterById(Number(characterId)));
+  }, [characterId, dispatch]);
 
   return (
     <section>
       <h2>Character</h2>
+      <div>{comics && comics.map(comic => <ComicItem key={comic.id} />)}</div>
     </section>
   );
 };
