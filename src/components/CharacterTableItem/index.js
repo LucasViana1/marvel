883720e7d12aac8a7cde26ca8d9c56ec1dad8311
Lucas Viana <lucasviana112@gmail.com>
2020-07-06 09:30:26 -0300

    created navigation to character details

--- src/components/CharacterTableItem/index.js
+++ src/components/CharacterTableItem/index.js
@@ -2,19 +2,31 @@
 import React from 'react';
 import Moment from 'react-moment';
 
+import { useHistory } from 'react-router-dom';
 import { TableRow, BodyText } from './styles';
 
 type Props = {
   character: {
+    id: number,
     name: string,
     description: string,
     modified: string,
+    thumbnail: {
+      path: string,
+      extension: string,
+    },
   },
 };
 
 const CharacterTableItem = ({ character }: Props) => {
+  const history = useHistory();
+
+  function handleNavigateToCharacter() {
+    history.push(`/character/${character.id}`);
+  }
+
   return (
-    <TableRow>
+    <TableRow onClick={() => handleNavigateToCharacter()}>
       <BodyText>{character.name}</BodyText>
       <BodyText>{character.description}</BodyText>
       <BodyText>
