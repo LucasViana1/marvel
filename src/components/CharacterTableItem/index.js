668b060083b83f8d1d7d0338648d90b6d6f299c6
Lucas Viana <lucasviana112@gmail.com>
2020-07-06 15:24:17 -0300

    alter description style

--- src/components/CharacterTableItem/index.js
+++ src/components/CharacterTableItem/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Moment from 'react-moment';
 
 import { useHistory } from 'react-router-dom';
-import { TableRow, BodyText } from './styles';
+import { TableRow, BodyText, BodyTextDescription } from './styles';
 
 type Props = {
   character: {
@@ -28,7 +28,9 @@ const CharacterTableItem = ({ character }: Props) => {
   return (
     <TableRow onClick={() => handleNavigateToCharacter()}>
       <BodyText>{character.name}</BodyText>
-      <BodyText>{character.description}</BodyText>
+      <BodyTextDescription>
+        {character.description || 'No description found!'}
+      </BodyTextDescription>
       <BodyText>
         <Moment date={character.modified} format="DD/MM/YYYY hh:mm" />
       </BodyText>
--- src/components/CharacterTableItem/styles.js
+++ src/components/CharacterTableItem/styles.js
@@ -1,6 +1,26 @@
 import styled from 'styled-components';
 
-export const TableRow = styled.tr``;
+export const TableRow = styled.tr`
+  cursor: pointer;
+`;
 export const BodyText = styled.td`
-  border: 1px solid black;
+  border: 1px solid #a0a0a0;
+  padding: 6px 4px;
+  text-align: center;
+  width: 20%;
+  max-width: 10ch;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  color: rgb(0, 0, 0, 0.9);
+`;
+export const BodyTextDescription = styled.td`
+  border: 1px solid #a0a0a0;
+  padding: 6px 4px;
+  width: 60%;
+  max-width: 10ch;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  color: rgb(0, 0, 0, 0.9);
 `;
