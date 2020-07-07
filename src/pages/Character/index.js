766c53ec28f27c120fb8909347d00bef06aef1f1
Lucas Viana <lucasviana112@gmail.com>
2020-07-07 02:56:47 -0300

    added titles, header, new pagination, comic list

--- src/pages/Character/index.js
+++ src/pages/Character/index.js
@@ -1,60 +1,85 @@
 // @flow
 import * as React from 'react';
 // import { useEffect } from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { loadCharacterById } from '../../store/modules/characters/actions';
-import ComicItem from '../../components/ComicItem';
+
+import Pagination from '../../components/Pagination';
 
 import {
   Container,
   ContainerDetails,
   Details,
   ContainerComics,
+  ContainerImage,
+  Hr,
 } from './styles';
 
+import Header from '../../components/Header';
+import ListComics from '../../components/ListComics';
+
+import formatImageURL from '../../util/formatImageURL';
+
 const Character = () => {
-  const [imageURL, setImageURL] = useState('');
+  // const [imageURL, setImageURL] = useState('');
 
   const { characterId } = useParams();
 
   const dispatch = useDispatch();
 
-  const { comics, characterDetails } = useSelector(state => state.characters);
+  const { characterDetails } = useSelector(state => state.characters);
 
-  console.log('useSelector(state => state.characters)');
-  console.log(characterDetails);
+  // console.log('useSelector(state => state.characters)');
+  // console.log(characterDetails);
   // console.log(characterDetails.thumbnail);
   // console.log(characterDetails.thumbnail.path);
 
   useEffect(() => {
     // DISPATCH CHARACTER BY ID AND COMICS
     dispatch(loadCharacterById(Number(characterId)));
-  }, [characterId, dispatch]);
-
-  useEffect(() => {
-    if (characterDetails.thumbnail !== undefined) {
-      const { path, extension } = characterDetails.thumbnail;
-      setImageURL(`${path}.${extension}`);
-    }
-  }, [characterDetails]);
+  }, [characterId]);
 
   return (
-    <Container>
-      <ContainerDetails>
-        {characterDetails && <img src={imageURL} alt={characterDetails.name} />}
-        <Details>
-          <span>{`Nome: ${characterDetails.name}`}</span>
-          <p>{`Descrição: ${characterDetails.description}`}</p>
-        </Details>
-      </ContainerDetails>
-      <hr />
-      <ContainerComics>
-        {comics &&
-          comics.map(comic => <ComicItem key={comic.id} comic={comic} />)}
-      </ContainerComics>
-    </Container>
+    <>
+      <Header pathBack="/home" />
+      {console.log('RENDER COMICS 1')}
+      <Container>
+        <h1>Detalhes do personagem</h1>
+        {console.log('RENDER COMICS 2')}
+        <ContainerDetails>
+          {console.log('RENDER COMICS 3')}
+          <ContainerImage>
+            {characterDetails.thumbnail !== undefined && (
+              <img
+                src={formatImageURL(characterDetails.thumbnail)}
+                alt={characterDetails.name}
+              />
+              // <img src={imageURL} alt={characterDetails.name} />
+            )}
+          </ContainerImage>
+
+          <Details>
+            {console.log('RENDER COMICS 4')}
+            <h3>{`Nome: ${characterDetails.name}`}</h3>
+            <p>
+              {`Descrição: ${
+                characterDetails.description || 'No description found!'
+              }`}
+            </p>
+          </Details>
+        </ContainerDetails>
+        <Hr />
+        <ContainerComics>
+          {console.log('RENDER COMICS 5')}
+          <h2>Fascículos</h2>
+
+          <ListComics />
+        </ContainerComics>
+        <Pagination comicCharacterSearchById={Number(characterId)} />
+      </Container>
+    </>
   );
 };
 
--- src/pages/Character/styles.js
+++ src/pages/Character/styles.js
@@ -8,18 +8,38 @@ export const Container = styled.section`
   /* height: 50vh; */
   /* margin-top: 70px; */
   width: 100%;
+
+  h1 {
+    margin: 20px 0;
+  }
 `;
 export const ContainerDetails = styled.div`
   display: flex;
-  width: 90%;
+  width: 100%;
   padding: 12px;
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+  }
+
+  h3 {
+    text-align: center;
+    margin: 10px 2px;
+  }
+`;
+export const ContainerImage = styled.div`
+  display: flex;
+  flex: 1;
+  width: 100%;
   img {
-    width: 40%;
+    width: 100%;
+    /* width: 40%; */
     max-height: 100%;
     object-fit: cover;
   }
 `;
 export const Details = styled.article`
+  flex: 3;
   flex-direction: column;
   width: 100%;
   background: #fff;
@@ -32,5 +52,13 @@ export const Details = styled.article`
 export const ContainerComics = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top: 42px;
+  align-items: center;
+  /* margin-top: 42px; */
+
+  h2 {
+    margin: 18px 2px;
+  }
+`;
+export const Hr = styled.hr`
+  width: 100%;
 `;
